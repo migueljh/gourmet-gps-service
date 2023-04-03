8a4ef8244c70ac7eba84d051239241122f10ff40
migueljh <miguelhernandezjp@gmail.com>
2023-04-03 18:36:01 -0300

    Change sort parameter in get restaurants endpoint to sort by rating

--- server.ts
+++ server.ts
@@ -26,7 +26,7 @@ app.get("/api/restaurants", async (req: Request, res: Response) => {
         latitude,
         longitude,
         limit: 10,
-        sort_by: "best_match",
+        sort_by: "rating",
       },
     });
     res.json(response.data);
